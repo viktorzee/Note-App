7ddf2af97d787db874f2dddb233713d266114e3c
Victor Opara <opara.victor@gmail.com>
2021-11-07 12:34:20 +0100

    updates on stykles and functionality

--- scripts/notes-functions.js
+++ scripts/notes-functions.js
@@ -26,18 +26,9 @@ const removeNotes = (id) => {
 
 //generate note dom
 const generateNotesDom = (note) => {
-    const noteEl = document.createElement('div');
-    const textEl = document.createElement('a');
-    const removeButton = document.createElement('button');
-
-    //set up the delete button
-    removeButton.textContent = 'x'
-    noteEl.appendChild(removeButton)
-    removeButton.addEventListener('click', function () {
-        removeNotes(note.id);
-        savedNotes(notes)
-        renderNotes(notes, filters)
-    })
+    const noteEl = document.createElement('a');
+    const textEl = document.createElement('p');
+    const statusEl = document.createElement('p')
 
     //set up the note title text
     if (note.title.length > 0) {
@@ -45,8 +36,17 @@ const generateNotesDom = (note) => {
     } else {
         textEl.textContent = 'Unnamed Note'
     }
-    textEl.setAttribute('href', `./edit.html#${note.id}`)
+    textEl.classList.add('list-items__title')
     noteEl.appendChild(textEl)
+
+    //setup  the link
+    textEl.setAttribute('href', `./edit.html#${note.id}`)
+    noteEl.classList.add('list-item')
+
+    //setup status message
+    statusEl.textContent = generateLastEdited(note.updatedAt);
+    statusEl.classList.add('list-otem__subtitle')
+    noteEl.appendChild(statusEl);
 
     return noteEl
 }
@@ -76,17 +76,41 @@ const sortNotes = (notes, sortBy) => {
 
 //render application
 const renderNotes = (notes, filters) => {
+    const notesEl =     document.querySelector('#notes')
     note = sortNotes(notes, filters.sortBy)
     const filteredNotes = notes.filter(note => note.title.toLowerCase().includes(filters.searchText.toLowerCase())
     )
 
-    document.querySelector('#notes').innerHTML = ''
+    notesEl.innerHTML = ''
 
-    filteredNotes.forEach(note => {
-        const noteEl = generateNotesDom(note)
-        document.querySelector('#notes').appendChild(noteEl);
-    })
+
+    if (filteredNotes.length > 0) {
+        filteredNotes.forEach(note => {
+            const noteEl = generateNotesDom(note)
+            notesEl.appendChild(noteEl);
+        })
+    } else{
+        const emptyMessage = document.createElement('p');
+        emptyMessage.textContent = 'No Notes to show'
+        emptyMessage.classList.add('empty-message')
+        notesEl.appendChild(emptyMessage)
+    }
+
+  
 }
 
 //generated Last edited
 const generateLastEdited = (timestamp) => `Last edited ${moment(timestamp).fromNow()}`
+
+/*     
+    const removeButton = document.createElement('button');
+
+    //set up the delete button
+    removeButton.textContent = 'x'
+    noteEl.appendChild(removeButton)
+    removeButton.addEventListener('click', function () {
+        removeNotes(note.id);
+        savedNotes(notes)
+        renderNotes(notes, filters)
+    })
+*/
